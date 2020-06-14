d898a6484ad2a2543eb617380998c3749fc02379
v1va <vivabrolite@gmail.com>
2020-06-14 05:20:02 -0700

    Initial Commit

---
+++ md2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  md2
+//
+//  Created by viva brolite on 14/06/2020.
+//  Copyright © 2020 vivabrolite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
