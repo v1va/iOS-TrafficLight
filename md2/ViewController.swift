48d8150ad75958054d08cebe3dc0645052256758
v1va <vivabrolite@gmail.com>
2020-06-18 13:03:07 -0700

    Change the color and label with datepicker

--- md2/ViewController.swift
+++ md2/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var circle: UIImageView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var colorLabel: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        circle.tintColor = .gray
+        datePicker.datePickerMode = UIDatePicker.Mode.time
     }
+    
+    
+    @IBAction func timeChanged(_ sender: UIDatePicker) {
+        
+        let unixTime = sender.date.timeIntervalSince1970
 
-
+        let unixTimeInt:Int = Int(unixTime)
+        let time = (unixTimeInt-10800)%420
+        print(time)
+        
+        if time <= 60 {
+            circle.tintColor = .green
+            colorLabel.text = "U should GO!"
+            colorLabel.textColor = .green
+        }
+        
+        if time == 120{
+            circle.tintColor = .yellow
+            colorLabel.text = "U should WAIT!"
+            colorLabel.textColor = .yellow
+        }
+        
+        if time > 120 && time < 360{
+            circle.tintColor = .red
+            colorLabel.text = "U should STOP!"
+            colorLabel.textColor = .red
+        }
+        
+        if time == 360{
+            circle.tintColor = .yellow
+            colorLabel.text = "U should WAIT!"
+            colorLabel.textColor = .green
+        }
+    }
+    
+    
 }
-
